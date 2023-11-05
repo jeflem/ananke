5970b97491a6fe596980a85a2dc95ab68e38968d
Jens Flemming <jens.flemming@fh-zwickau.de>
2023-11-05 06:24:44 +0100

    Add script for jupyterfs install

---
+++ images/ananke-base/assets/install/jupyterfs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source /opt/conda/etc/profile.d/conda.sh
+
+conda activate jhub
+
+# development version of webdav-client
+# has bug fix for
+# sed -i 's/root=self.webdav.root, path=urn.path()/root=unquote(self.webdav.root), path=urn.path()/' \
+#     /opt/conda/envs/jhub/lib/python3.11/site-packages/webdav3/client.py
+pip install git+https://github.com/ezhov-evgeny/webdav-client-python-3.git@98c23d1abd15efc3db9cfc756429f00041578bc2
+
+# install jupyter-fs from fork that does not use async defs
+# cf. https://github.com/jpmorganchase/jupyter-fs/issues/181
+pip install git+https://github.com/jeflem/jupyter-fs.git@ananke_0.3
+
+# WebDAV support for jupyterfs
+pip install fs.webdavfs
+
+# jupyterfs configuration
+# note https://github.com/jpmorganchase/jupyter-fs/issues/180
+cp /opt/install/jupyter_server_config.py /opt/conda/envs/jhub/etc/jupyter/jupyter_server_config.py
