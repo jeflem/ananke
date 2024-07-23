830dfe47854ea672c1d3db2ef436e51404eeb9ab
Marcus Wittig <marcus.wittig@fh-zwickau.de>
2024-07-23 08:41:43 +0200

    Moved utility functions into misc/.

--- images/ananke-nbgrader/assets/kore/misc/make_keys.py
+++ images/ananke-nbgrader/assets/kore/misc/make_keys.py
@@ -1,33 +1,34 @@
-# create new key pair for LTI communication
+# Used to create new key pair for LTI communication.
+# TODO add try - except block to open() statement?
 
 import json
 
 from Crypto.PublicKey import RSA
 from jwcrypto.jwk import JWK
 
-key_dir = 'keys'
+key_dir = '/opt/kore/keys'
 key_name = 'lti_key'
 
-# create key pair
+# Create key pair.
 key = RSA.generate(4096)
 private_key = key.exportKey()
 public_key = key.publickey().exportKey()
 
-# write private key to file
+# Write private key to file.
 with open(f'{key_dir}/{key_name}', 'wb') as f:
     f.write(private_key)
 
-# write public key
+# Write public key.
 with open(f'{key_dir}/{key_name}.pub', 'wb') as f:
     f.write(public_key)
 
-# make key set
+# Make key set.
 jwk_obj = JWK.from_pem(public_key)
 public_jwk = json.loads(jwk_obj.export_public())
 public_jwk['alg'] = 'RS256'
 public_jwk['use'] = 'sig'
 public_jwk_str = json.dumps(public_jwk)
 
-# write key set
+# Write key set.
 with open(f'{key_dir}/{key_name}.json', 'w') as f:
     f.write(public_jwk_str)
