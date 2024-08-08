79d5029eb9ad87f4467263a82155b4b38dd1294e
Marcus Wittig <marcus.wittig@fh-zwickau.de>
2024-08-08 10:36:49 +0200

    Refactored problems_route.py.

--- images/ananke-nbgrader/assets/kore/routes/problems_route.py
+++ images/ananke-nbgrader/assets/kore/routes/problems_route.py
@@ -1,15 +1,15 @@
 import json
 import logging
-import os
 import time
+from pathlib import Path
 from subprocess import run, CalledProcessError
 
 from flask import Blueprint, Response, current_app
 from flask import request as flask_request
 
-from misc.utils import get_list
+from exceptions import InfoFileError
+from misc.utils import get_list, load_info
 from models.enums import Content
-from models.lti_file_reader import LTIFileReader
 
 problems_bp = Blueprint('problems', __name__)
 
@@ -28,53 +28,31 @@ def problems():
         except ValueError:
             return Response(response=json.dumps({'message': 'ValueError'}), status=500)
 
-
-
-
-
+    # Copy a problem.
     if flask_request.method == 'POST':
         try:
-            user_name, path = flask_request.json['user'], flask_request.json['path'].removesuffix('/')
-            logging.debug(f'User: {user_name}')
-            logging.debug(f'Path of problem to be copied: {path}')
+            src = flask_request.json['fromPath'].removesuffix('/')
+            dst = flask_request.json['toPath'].removesuffix('/')
         except KeyError:
             logging.error('Request key is not in form!')
             return Response(response=json.dumps({'message': 'KeyError'}), status=500)
 
-        logging.info(f'User {user_name} is copying problem from {path}.')
-
-        # Read and parse JSON file containing LTI data of current user.
-        lti_file_reader: LTIFileReader = LTIFileReader(user_name=user_name, file_path=f'runtime/lti_{user_name}.json')
-        lti_file_reader.read_file()
-        lti_file_reader.extract_values()
-
-        if lti_file_reader.read_success and lti_file_reader.parse_success:
-            course_id, course_title, grader_user = lti_file_reader.course_id, lti_file_reader.course_title, lti_file_reader.grader_user
-        else:
-            return lti_file_reader.error_response
-
-        # Get name of problem to be copied.
-        problem_name = path.split('/')[-1].removesuffix('.ipynb')
-        logging.debug(f'Problem name: {problem_name}')
-
-        # Check if the folder for imported problems exists.
-        if not os.path.isdir(f'/home/{grader_user}/course_data/source/imported_problems/'):
-            try:
-                run(['mkdir', '-p', f'/home/{grader_user}/course_data/source/imported_problems/'], check=True)
-                run(['chown', f'{grader_user}:{grader_user}', f'/home/{grader_user}/course_data/source/imported_problems/'], check=True)
-            except CalledProcessError:
-                logging.error('Command cannot be executed!')
-                return Response(response=json.dumps({'message': 'CalledProcessError'}), status=500)
-
-        # Copy problem with timestamp and change ownership.
-        actual_time = time.strftime(date_time_format)
-        dst = f'/home/{grader_user}/course_data/source/imported_problems/{problem_name} ({actual_time}).ipynb'
-
+        # Read `info.json` file.
         try:
-            run(['cp', f'{path}', dst], check=True)
-            run(['chown', f'{grader_user}:{grader_user}', dst], check=True)
+            info = load_info(f'{dst}/info.json')
+            grader_user = info['grader_user']
+        except KeyError:
+            return Response(response=json.dumps({'message': 'KeyError'}), status=500)
+        except InfoFileError:
+            return Response(response=json.dumps({'message': 'InfoFileError'}), status=500)
+
+        dst = f'{dst}/source/imported/'
+        filename = f'{Path(src).stem}_{time.strftime(date_time_format)}{Path(src).suffix}'
+        try:
+            run(['mkdir', '-p', dst], check=True)
+            run(['cp', src, f'{dst}{filename}'], check=True)
+            run(['chown', '-R', f'{grader_user}:{grader_user}', f'{dst}{filename}'], check=True)
         except CalledProcessError:
-            logging.error('Command cannot be executed!')
             return Response(response=json.dumps({'message': 'CalledProcessError'}), status=500)
 
         return Response(response=json.dumps({'message': 'Selected problem copied successfully! \n'
