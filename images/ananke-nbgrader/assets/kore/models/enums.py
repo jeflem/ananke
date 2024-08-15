8aebc42dfbfd87b6da06cb00435a2fe22aeeb916
Marcus Wittig <marcus.wittig@fh-zwickau.de>
2024-08-15 15:23:49 +0200

    Added courses/current route and configuration for grading.

--- images/ananke-nbgrader/assets/kore/models/enums.py
+++ images/ananke-nbgrader/assets/kore/models/enums.py
@@ -9,11 +9,13 @@ class Subset(Enum):
     - `ALL`: Represents the entire collection. Used for retrieving the complete list of courses including all active/running courses as well as all backed up courses.
     Currently not used in code.
     - `ACTIVE`: Represents the subset indicating all active/running courses. This list will be used for course deletion.
+    - `CURRENT`: Represents the current course (the course the user accessed JupyterHub from). This list will be used for grading, when grading_scope is current (see config.json).
     - `OTHER`: Represents the subset indicating a list of all courses except the one the user is currently
 
     """
     ALL = auto()
     ACTIVE = auto()
+    CURRENT = auto()
     OTHER = auto()
 
 
