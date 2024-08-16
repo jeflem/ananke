1fb2e569db5bfb6f7dda51e3ebbcab9e9a270c69
Jens Flemming <jens.flemming@fh-zwickau.de>
2024-08-16 07:35:42 +0200

    Update jupyter-collaboration

--- images/ananke-base/assets/install/rtc.sh
+++ images/ananke-base/assets/install/rtc.sh
@@ -3,7 +3,8 @@
 source /opt/conda/etc/profile.d/conda.sh
 
 conda activate jhub
-pip install jupyter-collaboration==2.0.2
+pip install jupyter-collaboration==3.0.0b2
 
 # disabel RTC for all users
 jupyter labextension disable @jupyter/collaboration-extension
+jupyter labextension unlock @jupyter/collaboration-extension
