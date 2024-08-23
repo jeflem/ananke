3d0c2b8da40cef5fc604b48cde5528d75ceb8198
Marcus Wittig <marcus.wittig@fh-zwickau.de>
2024-08-23 11:04:24 +0200

    Added config.sh scripts.

---
+++ ananke-base-hub/config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Constants and Configuration
+# ---------------------------
+
+export CONTAINER_NAME=ananke-base-hub
+
+export USER_HOME_ROOT="/var/lib/private"
---
+++ ananke-nbgrader-hub/config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Constants and Configuration
+# ---------------------------
+
+export CONTAINER_NAME=ananke-nbgrader-hub
+
+export USER_HOME_ROOT="/var/lib/private"
+export GRADER_HOME_ROOT="/home"
+export NBGRADER_EXCHANGE_ROOT="/opt/nbgrader_exchange"
+
+export GPU_YAML_FILE="/etc/cdi/nvidia.yaml"
