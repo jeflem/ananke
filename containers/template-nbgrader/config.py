3f349eda4a7f440264fd379cbf5efcd7f26d477f
Jens Flemming <jens.flemming@fh-zwickau.de>
2024-08-26 12:32:46 +0200

    Update container definition templates

---
+++ containers/template-nbgrader/config.py
@@ -0,0 +1,14 @@
+# config file for creating an Ananke container
+
+config['image_name'] = 'ananke-base:latest'
+config['port'] = 8000
+
+# additional arguments to Podman
+config['podman_args'] = []
+config['podman_args'].append('-m=8g')  # limit memory to 8 GB per user
+
+# host directories accessible inside the container
+# (paths have to be absolute or relative to config file path)
+config['volumes'] = []
+config['volumes'].append(('kore', '/opt/kore/runtime'))
+config['volumes'].append(('nbgrader_exchange', '/opt/nbgrader_exchange'))
