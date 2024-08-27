6698a3745f06f17be48ccfd8e9277980767c2c15
Jens Flemming <jens.flemming@fh-zwickau.de>
2024-08-27 05:42:28 +0200

    Add config option cpus to container definitions

--- containers/template-base/config.py
+++ containers/template-base/config.py
@@ -6,6 +6,9 @@ config['port'] = 8000
 # memory limit for container ('interactive', '8g', '500m',...)
 config['memory'] = 'interactive'
 
+# maximum number of CPUs (cores) available to the container ('interactive', 1, 2,..., 'max')
+config['cpus'] = 'interactive'
+
 # additional arguments to Podman
 config['podman_args'] = []
 
