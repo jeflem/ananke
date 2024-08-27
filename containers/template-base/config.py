47d323e6840612aaaa8b345fec1ed547f97b3c43
Jens Flemming <jens.flemming@fh-zwickau.de>
2024-08-27 07:46:38 +0200

    Add more hints for user

--- containers/template-base/config.py
+++ containers/template-base/config.py
@@ -1,6 +1,9 @@
 # config file for creating an Ananke container
 
+# Ananke image to use ("./ananke list" shows available ones)
 config['image_name'] = 'ananke-base:latest'
+
+# port the container is accessible through (ask your host admin for this value)
 config['port'] = 8000
 
 # memory limit for container ('interactive', '500m', '8g',..., 'max')
@@ -12,9 +15,9 @@ config['cpus'] = 'interactive'
 # GPUs available to the container ('interactive', GPU name (string), list of GPU names (strings))
 config['gpus'] = 'interactive'
 
-# additional arguments to Podman
+# additional arguments to Podman (list of strings)
 config['podman_args'] = []
 
-# host directories accessible inside the container
-# (paths have to be absolute or relative to config file path)
+# host directories accessible inside the container (list of (host_path, container_path) tuples)
+# paths have to be absolute or relative to config file path)
 config['volumes'] = []
--- containers/template-nbgrader/config.py
+++ containers/template-nbgrader/config.py
@@ -1,6 +1,9 @@
 # config file for creating an Ananke container
 
-config['image_name'] = 'ananke-base:latest'
+# Ananke image to use ("./ananke list" shows available ones)
+config['image_name'] = 'ananke-nbgrader:latest'
+
+# port the container is accessible through (ask your host admin for this value)
 config['port'] = 8000
 
 # memory limit for container ('interactive', '500m', '8g',..., 'max')
@@ -12,11 +15,11 @@ config['cpus'] = 'interactive'
 # GPUs available to the container ('interactive', GPU name (string), list of GPU names (strings))
 config['gpus'] = 'interactive'
 
-# additional arguments to Podman
+# additional arguments to Podman (list of strings)
 config['podman_args'] = []
 
-# host directories accessible inside the container
-# (paths have to be absolute or relative to config file path)
+# host directories accessible inside the container (list of (host_path, container_path) tuples)
+# paths have to be absolute or relative to config file path)
 config['volumes'] = []
 config['volumes'].append(('kore', '/opt/kore/runtime'))
 config['volumes'].append(('nbgrader_exchange', '/opt/nbgrader_exchange'))
