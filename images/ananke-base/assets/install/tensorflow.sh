39ed4cccaa2099af913ca491584f8a50908a4681
Jens Flemming <jens.flemming@fh-zwickau.de>
2024-08-27 08:36:35 +0200

    Downgrade NumPy in TensorFlow install script via Conda and remove downgrade of Python

--- images/ananke-base/assets/install/tensorflow.sh
+++ images/ananke-base/assets/install/tensorflow.sh
@@ -4,11 +4,8 @@ source /opt/conda/etc/profile.d/conda.sh
 
 conda activate python3
 
-# downgrade Python
-conda install -y python=3.10
-
 # install
-pip install numpy==1.26.4
+conda install numpy==1.26.4
 pip install tensorflow[and-cuda]==2.17.0
 pip install keras_tuner
 
