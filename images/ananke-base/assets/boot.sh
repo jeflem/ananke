e5f6f8e9533d4e1fee91e974d94b3204eaf91fa6
Jens Flemming <jens.flemming@htw-dresden.de>
2026-05-07 05:13:22 +0200

    Use Miniforge in base image and improve time zone and cert handling

--- images/ananke-base/assets/boot.sh
+++ images/ananke-base/assets/boot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-timedatectl set-timezone Europe/Berlin
+timedatectl set-timezone $TZ
+
+test -e /opt/ca.pem && cp /opt/ca.pem /usr/local/share/ca-certificates/ca.crt && update-ca-certificates
 
 # make JupyterHub cookie secret
 touch /opt/conda/envs/jhub/etc/jupyterhub/jupyterhub_cookie_secret && \
