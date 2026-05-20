a1bb164aef2d6f6afeb554bf926c0b828a7803eb
Jens Flemming <jens.flemming@htw-dresden.de>
2026-05-20 07:47:35 +0200

    Add imprint aso. to doc footer

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -15,3 +15,7 @@ myst_heading_anchors = 4
 html_static_path = ['_static']
 
 html_theme = 'sphinx_book_theme'
+
+html_theme_options = {
+    'extra_footer': '<div><a href="https://www2.htw-dresden.de/~fjeme691/flemming/impressum.html" target="_blank" rel="noopener noreferrer">Imprint</a> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="https://www2.htw-dresden.de/~fjeme691/flemming/barrierefreiheit.html" target="_blank" rel="noopener noreferrer">Accessibility</a> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="https://www2.htw-dresden.de/~fjeme691/flemming/datenschutz.html" target="_blank" rel="noopener noreferrer">Data privacy</a></div>',
+}
